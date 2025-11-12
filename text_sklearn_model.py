c4dda33675799c611c37de1d5869b236d9eece1f
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-11-12 15:04:43 +0200

    finished getAnswers

--- text_sklearn_model.py
+++ text_sklearn_model.py
@@ -9,4 +9,17 @@ class TextModel:
         self.cleaned_text = [res for s in self.text_arr if (res := s.strip())]
         self.vectorizer = TfidfVectorizer()
         self.embeddings_text = self.vectorizer.fit_transform(self.cleaned_text).toarray()
+
+    def getAnswers(self, question: str, nAnswers: int) -> list[str]:
+        query_embedding = self.vectorizer.transform([question]).toarray()
+        similarities = cosine_similarity(self.embeddings_text, query_embedding).flatten()
+        sorted_indices = np.argsort(similarities)[::-1]
+
+        result = []
+        for i in sorted_indices:
+            if similarities[i] > 0:
+                result.append(self.cleaned_text[i])
+                if len(result) == nAnswers:
+                    break
         
+        return result
